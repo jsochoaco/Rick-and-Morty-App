6492886eaba2a42fc5c8dc6bb0582847f44bc233
jsochoaco <sebastianochoa05@gmail.com>
2023-07-26 13:13:14 -0500

    26julio bug

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,7 +7,7 @@ const initialState = {
 
 
 export const reducer = (state=initialState, action) => {
-    switch (action.key) {
+    switch (action.type) {
         case ADD_FAVORITES:
             return {
                 ...state,
